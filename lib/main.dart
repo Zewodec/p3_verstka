b8d62e3bc380fe4c5b0d83ac4312da695e2e10f8
Zewodec <zewodec@gmail.com>
2023-03-10 04:22:14 +0200

    Base macket of instagram

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Verstka',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: const MyHomePage(title: 'Verstka project'),
     );
@@ -29,38 +29,101 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.,  // Set Color of bg
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Wrap(spacing: 20, children: const [
+            SizedBox(
+              width: 70,
+              height: 70,
+              child: CircleAvatar(
+                backgroundColor: Colors.teal,
+                child: Icon(Icons.face_2),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(
+              width: 70,
+              height: 70,
+              child: CircleAvatar(
+                backgroundColor: Colors.yellow,
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+            SizedBox(
+              width: 70,
+              height: 70,
+              child: CircleAvatar(
+                backgroundColor: Colors.amber,
+              ),
+            ),
+            SizedBox(
+              width: 70,
+              height: 70,
+              child: CircleAvatar(
+                backgroundColor: Colors.green,
+              ),
+            ),
+          ]),
+          const SizedBox(
+            height: 5,
+          ),
+          Column(
+            children: [
+              Row(
+                children: const [
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Text("@Zewodec"),
+                  Spacer(),
+                  Icon(Icons.menu),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Image.asset(
+                "assets/images/post_1.jpg",
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Row(
+                children: const [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(Icons.favorite),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Icon(Icons.comment),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Icon(Icons.send),
+                  Spacer(),
+                  Icon(Icons.bookmark),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
